7e78d0638de6206e749fe205181ed80293bad124
adminNIES <adminNIES@192.168.8.101>
2017-03-25 22:28:35 -0600

    se borro todo solo dejare hola mundo por el momento

--- src/proyectcolaborativo/ProyectColaborativo.java
+++ src/proyectcolaborativo/ProyectColaborativo.java
@@ -9,9 +9,6 @@ public class ProyectColaborativo {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        System.out.println("Bienvenido");
-        System.out.println("modificado por william111111111!!!");
-        System.out.println("otra mensaje :)");
     }
     
 }
