3dd65ab765900cb676b6ba53b194c16395a6aba0
adminNIES <adminNIES@192.168.8.101>
2017-03-25 18:50:48 -0600

    proyecto creado para prueba de conexion

---
+++ src/proyectcolaborativo/ProyectColaborativo.java
@@ -0,0 +1,18 @@
+
+package proyectcolaborativo;
+
+/**
+ *
+ * @author adminNIES
+ */
+public class ProyectColaborativo {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo");
+        System.out.println("Bienvenido");
+        System.out.println("Adelante!!!");
+    }
+    
+}
