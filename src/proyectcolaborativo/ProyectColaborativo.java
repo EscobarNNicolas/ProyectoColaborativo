cdc3968b94c90ccf450bd19ed39c00547b16d6ee
adminNIES <adminNIES@192.168.8.101>
2017-03-25 22:13:29 -0600

    borre linea de *

--- src/proyectcolaborativo/ProyectColaborativo.java
+++ src/proyectcolaborativo/ProyectColaborativo.java
@@ -10,7 +10,6 @@ public class ProyectColaborativo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Bienvenido");
-        System.out.println("***************************");
         System.out.println("esta es una prueba");
     }
     
